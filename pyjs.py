e0c387f9fe7d2649d05c7218a3eae05fa549edc5
Anand Tyagi <ananddtyagi@yahoo.com>
2019-02-19 23:00:32 +0800

    update

--- pyjs.py
+++ pyjs.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    return render_template('index.html')
 
 @app.route('/idiomList',methods = ['POST', 'GET'])
 def result():
