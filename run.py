395729622dedb397de7cc1a532f669fa188de7a8
Anand Tyagi <ananddtyagi@yahoo.com>
2019-03-19 22:22:53 +0800

    ^

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('idiomsList.html')
 
 @app.route('/',methods = ['POST', 'GET'])
 def result():
