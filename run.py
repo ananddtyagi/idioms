08057441848ae5d3529450bd0d16ab7c3fb28749
Anand Tyagi <ananddtyagi@yahoo.com>
2019-03-21 23:56:13 +0800

    ^

--- run.py
+++ run.py
@@ -1,22 +1,18 @@
 from lib.flask import Flask, render_template, request
 from idioms import findIdioms
-i = 0
+x =
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    if i == 0:
-        i = i + 1
-        return render_template('index.html', iList = ["iList", "lol", "hah","yeet"])
-    return render_template("index.html",iList = ["iList", "hello", "sup","damnnn"])
-
+    return render_template('index.html', iList = ["iList", "lol", "hah","yeet"])
 
 @app.route('/',methods = ['POST'])
 def result():
-    flash("WRSDFASDFSDF")
-    phrase = request.form['phrase']
-    iList = findIdioms(phrase)
-    return render_template("index.html",iList = ["iList", "hello", "sup","damnnn"])
+   if request.method == 'POST':
+      phrase = request.form['phrase']
+      iList = findIdioms(phrase)
+      return render_template("index.html",iList = ["iList", "hello", "sup","damnnn"])
 
 
 if __name__ == "__main__":
