2fad21b8f1955a3b3d1b88b301aa6fa9118aac74
Anand Tyagi <ananddtyagi@yahoo.com>
2019-03-21 22:31:55 +0800

    ^

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/idiomList',methods = ['POST'])
+@app.route('/',methods = ['POST'])
 def result():
    if request.method == 'POST':
       phrase = request.form['phrase']
