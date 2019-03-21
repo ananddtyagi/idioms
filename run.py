63069ad73cf7ec936e063dc6690b077c1f233466
Anand Tyagi <ananddtyagi@yahoo.com>
2019-03-22 00:34:05 +0800

    ^

--- run.py
+++ run.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', iList = ["iList", "lol", "hah","yeet"])
-def index(x):
+    return index(0)
     if x == 0:
+        return render_template('index.html', iList = ["iList", "lol", "hah","yeet"])
+    if x == 1:
         return render_template("index.html",iList = ["iList", "hello", "sup","damnnn"])
 
 @app.route('/',methods = ['GET', 'POST'])
 def result():
-    index(0)
+    index(1)
     if request.method == 'POST':
       phrase = request.form['phrase']
       iList = findIdioms(phrase)
