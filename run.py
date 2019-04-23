4fbc3ea82d484dda2b35e08decf0e3908c86aba5
Anand Tyagi <ananddtyagi@yahoo.com>
2019-04-23 18:55:21 +0800

    update

--- run.py
+++ run.py
@@ -5,21 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return index(0)
-def index(x):
-    if x == 0:
-        return render_template('index.html', iList = ["iList", "lol", "hah","yeet"])
-    if x == 1:
-        return render_template("index.html",iList = ["iList", "hello", "sup","damnnn"])
+    return render_template('index.html', iList = "")
 
 @app.route('/',methods = ['GET', 'POST'])
 def result():
     if request.method == 'POST':
       phrase = request.form['phrase']
-      print(phrase)
-      iList = findIdioms(phrase)
-      print(iList)
-      return render_template("index.html",iList = ["iList", "hello", "sup","damnnn"])
+      iList = ["lmao", "Swag"]
+      #iList = findIdioms(phrase)
+      #THE PROBLEM TAKES PLACE WITHIN THE GOOGLE TRANSLATE ITSELF BECAUSE I NEED A KEY, IT"S BLOCKING ME FROM MAKING A CALL FROM A WEBSITE BUT WORKS IF I JUST MAKE IT FROM A LOCAL HOST
+      return render_template("index.html",iList = iList)
 
 
 if __name__ == "__main__":
