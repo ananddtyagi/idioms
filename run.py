04574e37b783a89b2012eb5e67b1abc39bdecd69
Anand Tyagi <ananddtyagi@yahoo.com>
2019-07-11 11:25:44 -0400

    changed back to dynamic response

--- run.py
+++ run.py
@@ -11,8 +11,7 @@ def index():
 def result():
     if request.method == 'POST':
       phrase = request.form['phrase']
-      iList = ["lmao", "Swag"]
-      #iList = findIdioms(phrase)
+      iList = findIdioms(phrase)
       #THE PROBLEM TAKES PLACE WITHIN THE GOOGLE TRANSLATE ITSELF BECAUSE I NEED A KEY, IT"S BLOCKING ME FROM MAKING A CALL FROM A WEBSITE BUT WORKS IF I JUST MAKE IT FROM A LOCAL HOST
       return render_template("index.html",iList = iList)
 
